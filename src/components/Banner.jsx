a607034965d6b81cc62f946e3cdbd6dcdf76c139
kharoldcanova <kharoldsamir@gmail.com>
2023-01-10 12:35:24 -0500

    banner and improvements

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,26 +1,28 @@
 import { Col, Container, Row } from "react-bootstrap";
-import iconoLinkedin from "../assets/img/icon-linkedin.svg";
-export const Banner = () =>{
-    return (
-        <section className="banner">
-            <Container>
-                <Row>
-                    <Col xs={12} md={6} xl={7}>
-                        <h1 className="title-banner">
-                            Kharold Canova
-                        </h1>
-                        <br />
-                        <p>Mi proposito es desarrollar para solucionar problematicas de tematica social. Soy una persona autodidacta, curiosa y responsable. Interesado en las tecnologias actuales. Quiero seguir aprendiendo conociendo mas del mundo laboral</p>
-                        <br />
-                        <h4>Puedes conocer mas de mi trabajo en: </h4>
-                    </Col>
-                    <Row xs={5} md={3} xl={4}>
-                        <img className="social-icon" src={iconoLinkedin} alt="linkedin"/>
-                        <img className="social-icon" src={iconoLinkedin} alt="linkedin"/>
-                        <img className="social-icon" src={iconoLinkedin} alt="linkedin"/>
-                    </Row>
-                </Row>
-            </Container>
-        </section>
-    );
-}
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="justify-content-center align-items-center" >
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Kharold Canova</span>
+            <br />
+            <p>
+              Mi proposito es desarrollar para solucionar problematicas de
+              tematica social. Soy una persona autodidacta, curiosa y
+              responsable. Interesado en las tecnologias actuales. Quiero seguir
+              aprendiendo conociendo mas del mundo laboral
+            </p>
+            <span>Puedes conocer mas de mi trabajo en mi repositorio: </span>
+            <button>GitHub</button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+                <img src={headerImg} alt="Header img" />
+            </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,48 @@
-
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import iconLinkedin from "../assets/img/icon-linkedin.svg";
+import iconNotion from "../assets/img/icon-notion.svg";
+import iconGitHub from "../assets/img/icon-github.svg";
 
 export const Navigationbar = () => {
   return (
     <Navbar expand="lg" className="scrolled">
       <Container>
         {/* Title */}
-        <Navbar.Brand className='icon-text' href="#home">PORTAFOLIO</Navbar.Brand>
+        <Navbar.Brand href="/" className="icon-text">
+          PORTAFOLIO
+        </Navbar.Brand >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         {/* Items */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className='navbar-link' href="#home">Home</Nav.Link>
-            <Nav.Link className='navbar-link' href="#projects">Projectos</Nav.Link>
-            <Nav.Link className='navbar-link' href="#aboutme">Sobre mi</Nav.Link>
+            <Nav.Link className="navbar-link" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="navbar-link" href="#projects">
+              Projectos
+            </Nav.Link>
+            <Nav.Link className="navbar-link" href="#aboutme">
+              Sobre mi
+            </Nav.Link>
           </Nav>
+          <span className="navbar-text">
+          <div className="social-icon">
+            <a href="">
+              <img src={iconLinkedin} alt="" />
+            </a>
+            <a href="">
+              <img src={iconNotion} alt="" />
+            </a>
+            <a href="">
+              <img src={iconGitHub} alt="" />
+            </a>
+          </div>
+          <button className="vvd" onClick={() => console.log("connected")}>
+              <span>Contactame</span>
+            </button>
+        </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
