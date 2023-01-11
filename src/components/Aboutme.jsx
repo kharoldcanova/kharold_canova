5a0f8b250423a0b65a17abaa214dbfa6499a0ab1
kharoldcanova <kharoldsamir@gmail.com>
2023-01-11 17:53:32 -0500

    add links

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -34,25 +34,29 @@ export const Aboutme = () => {
         imgURL: iconMusic,
         title: "Escuchar musica",
         description: "Escucho generos variados prefieriendo el rock ademas del lofi",
-        textButton: "Ver Spotify"
+        textButton: "Ver Spotify",
+        link: "https://open.spotify.com/playlist/37i9dQZF1EuI548Sqd104M?si=4c16bd87275b4bf6"
     },
     {
         imgURL: iconMovie,
         title: "Peliculas de ciencia ficcion",
         description: "Peliculas de viajes en el tiempo o con trasfondo psicologico, tambien me encanta la comedia.",
-        textButton: "Ver Peliculas"
+        textButton: "Ver Peliculas",
+        link: "https://www.espinof.com/criticas/predestination-alucinante-pelicula-viajes-tiempo-netflix-capaz-sorprender-al-espectador-avispado"
     },
     {
         imgURL: iconIA,
         title: "Inteligencia artificial",
         description: "Interes en los ultimos avances de las tecnologias",
-        textButton: "Ver Noticias"
+        textButton: "Ver Noticias",
+        link: "https://www.bbc.com/mundo/topics/cwr9j26ddr5t"
     },
     {
         imgURL: iconWrite,
         title: "Escribir relatos",
         description: "Escribir algo de poesia o pensamientos cortos para despejar la mente.",
-        textButton: "Ver Relatos"
+        textButton: "Ver Relatos",
+        link: "https://flicker-jitterbug-7ba.notion.site/Relatos-cortos-ccde4d3e3e074b7aa6a5164527f71602"
     }
   ];
   return (
@@ -69,7 +73,7 @@ export const Aboutme = () => {
                     hobbies.map(
                         (hobbie, index)=>{
                             return (
-                                <HobbiesCard title={hobbie.title} imgURL={hobbie.imgURL} description={hobbie.description} textButton={hobbie.textButton} key={index}  />
+                                <HobbiesCard title={hobbie.title} imgURL={hobbie.imgURL} description={hobbie.description} textButton={hobbie.textButton} link={hobbie.link} key={index}  />
                             )
                         }
                     )
--- src/components/HobbiesCard.jsx
+++ src/components/HobbiesCard.jsx
@@ -1,12 +1,16 @@
 import { Button, Card, Container } from "react-bootstrap";
 
-export const HobbiesCard = ({ title, imgURL, description, textButton }) => {
+export const HobbiesCard = ({ title, imgURL, description, textButton, link }) => {
   return (
     <div className="item">
-          <h2 className="title-card">{title}</h2>
-          <img className="img-card" src={imgURL} />
-          <p className="text-card">{description}</p>
-          <Button variant="success" className="button-card">{textButton}</Button>
+      <h2 className="title-card">{title}</h2>
+      <img className="img-card" src={imgURL} />
+      <p className="text-card">{description}</p>
+      <a href={link}>
+        <Button variant="success" className="button-card">
+          {textButton}
+        </Button>
+      </a>
     </div>
   );
 };
